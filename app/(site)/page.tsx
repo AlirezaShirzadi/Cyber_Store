75a60c55ee2cfa7e76fd6628dd737a964507d8a6
Alireza Shirzadi <shirzadiwork@gmail.com>
2025-06-29 16:16:31 +0330

    Add "Why Us" section with images to homepage
    - Introduced "Why Us" section with a grid layout and headline.
    - Added responsive Next.js `Image` components for displaying images.
    - Included new assets `/why/1.png`, `/why/2.png`, `/why/3.png`, `/why/4.png`.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/Container/Container";
 import TestimonialSlider from "@/components/TestimonialSlider/TestimonialSlider";
 import StoreSlider from "@/components/StoreSlider/StoreSlider";
 import HeroSlider from "@/components/HeroSlider/HeroSlider";
+import Image from "next/image";
 
 export default function Home() {
 
@@ -29,8 +30,29 @@ export default function Home() {
             </Container>
 
             <section>
-                <HeroSlider />
+                <HeroSlider/>
             </section>
+
+            <Container>
+                <section className={`pt-8 pb-[113px]`}>
+                    <h2 className={`bg-radial from-[#C1C2EF] text-center text-secondary text-5xl py-1.5 font-bold to-transparent mb-[49px]`}>چرا
+                        ما؟</h2>
+                    <div className={`grid grid-cols-12 gap-6 max-w-[872px] mx-auto w-full`}>
+                        <div className={`col-span-12 lg:col-span-6`}>
+                            <Image className={`w-full object-cover`} src={`/why/1.png`} alt={'why1'} width={424} height={308} />
+                        </div>
+                        <div className={`col-span-12 lg:col-span-6`}>
+                            <Image className={`w-full object-cover`} src={`/why/2.png`} alt={'why2'} width={424} height={308} />
+                        </div>
+                        <div className={`col-span-12 lg:col-span-6`}>
+                            <Image className={`w-full object-cover`} src={`/why/3.png`} alt={'why3'} width={424} height={308} />
+                        </div>
+                        <div className={`col-span-12 lg:col-span-6`}>
+                            <Image className={`w-full object-cover`} src={`/why/4.png`} alt={'why4'} width={424} height={308} />
+                        </div>
+                    </div>
+                </section>
+            </Container>
         </>
     );
 }
